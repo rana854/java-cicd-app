6316c24643f1e0bec934b08bf337b31661b966c8
RanaTarek566 <ranatarekmahmoud566@gmail.com>
2025-07-23 19:45:17 +0200

    Initial commit

---
+++ src/main/java/com/example/springbootapp/SpringbootappApplication.java
@@ -0,0 +1,23 @@
+package com.example.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class SpringbootappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootappApplication.class, args);
+    }
+     @GetMapping("/")
+    public String home() {
+        return "Hello from Spring Boot on port 8085!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+}
